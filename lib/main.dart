d2f9aa0546ea0a31ee995624c5884480913a6742
wendyoon <babywendyoon@gmail.com>
2026-05-23 17:30:57 +0800

    Initial Flutter setup

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'pages/auth/login_page.dart';
+import 'pages/auth/register_page.dart';
+import 'pages/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      initialRoute: '/login',
+
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/home': (context) => const HomePage(),
+      },
+    );
+  }
+}
